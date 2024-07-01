8cc8ba3e492e419d32d9459d56dcda80199118b1
kekW <panicua12@gmail.com>
2024-07-01 22:11:40 +0300

    Quotes parse solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, asdict
+
+import requests
+from bs4 import BeautifulSoup as Bs
+
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -8,8 +15,53 @@ class Quote:
     tags: list[str]
 
 
+def single_quote_processing(quote: Bs) -> Quote:
+    text = quote.select_one(".text").get_text()
+    author = quote.select_one(".author").get_text()
+    tags = quote.select(".tag")
+    tags = [tag.get_text() for tag in tags]
+    return Quote(text, author, tags)
+
+
+def parse_quotes_page() -> [Quote]:
+    page_num = 1
+    page = requests.get(BASE_URL).content
+    soup = Bs(page, "html.parser")
+    quotes = soup.select(".quote")
+    print(f"Page {page_num}")
+
+    while soup.select_one(".next"):
+        page = requests.get(
+            BASE_URL + soup.select_one(".next > a")["href"]
+        ).content
+        soup = Bs(page, "html.parser")
+        quotes.extend(soup.select(".quote"))
+
+        if soup.select_one(".next > a"):
+            page_num = (
+                int(soup.select_one(".next > a")["href"].split("/")[-2]) - 1
+            )
+            print(f"Page {page_num}")
+
+        # last page
+        if page_num > 1 and soup.select_one(".next > a") is None:
+            print(f"Page {page_num + 1}")
+
+    return [single_quote_processing(quote) for quote in quotes]
+
+
+def write_quotes_to_csv(filename: str, quotes: list) -> None:
+    with open(filename, mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=["text", "author", "tags"])
+        writer.writeheader()
+
+        for quote in quotes:
+            quote_to_dict = asdict(quote)
+            writer.writerow(quote_to_dict)
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    write_quotes_to_csv(output_csv_path, parse_quotes_page())
 
 
 if __name__ == "__main__":
